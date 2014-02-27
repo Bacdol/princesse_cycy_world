3c00f059a0a96fe6d78f39344973bd3232025d3b
Cyrille Gourdin <cyrille.gourdin@dimelo.com>
2014-02-27 12:08:46 +0100

    update production environment configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,7 @@ PrincesseCycyWorld::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+  config.serve_static_assets = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -59,7 +60,7 @@ PrincesseCycyWorld::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( *.* )
+  # config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
